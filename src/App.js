2b60016c99fcad27441fa845eefd5d4eb42f8795
Ollie <myself.ollie.lee@gmail.com>
2020-10-28 15:44:51 +1100

    menu item finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
-import Homepage from './pages/homepage/homepage.component'
-import './App.css'; 
+import Homepage from "./pages/homepage/homepage.component";
+import "./App.css";
 
 function App() {
   return (
-    <div >
+    <div>
       <Homepage />
     </div>
   );
--- src/components/menu-item/MenuItem.component.jsx
+++ src/components/menu-item/MenuItem.component.jsx
@@ -9,13 +9,14 @@ export default function MenuItem({ title, sneaker, imageUrl, big }) {
   };
   const style = { backgroundImage: `url(${imageUrl})` };
   return (
-    <div
-      className={`menu-item ${sneaker && "menu-sneakers"} ${big && "big"}`}
-      style={style}
-    >
-      <div className='content'>
-        <h1 className='title'>{title}</h1>
-        <span className='subtitle'>SHOP NOW</span>
+    <div className={`menu-item ${sneaker && "menu-sneakers"} ${big && "big"}`}>
+      <div className='background-image' style={style}>
+        {/* decouple the background image from the menu item,
+        we only need to the background image to increase*/}
+        <div className='content'>
+          <h1 className='title'>{title.toUpperCase()}</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
       </div>
     </div>
   );
